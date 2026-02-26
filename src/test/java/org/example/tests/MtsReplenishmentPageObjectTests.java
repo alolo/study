e960c06dddb20dcc848bd18e814bf8902cd047f6
Alexey <alo32@yandex.ru>
2026-02-26 13:03:00 +0500

    Lesson_10 modified xpath

--- src/test/java/org/example/tests/MtsReplenishmentPageObjectTests.java
+++ src/test/java/org/example/tests/MtsReplenishmentPageObjectTests.java
@@ -1,12 +1,27 @@
 package org.example.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Step;
 import org.example.pages.MtsTopUpBlockPage;
 import org.example.pages.PaymentIframePage;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.TmsLink;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.SeverityLevel.*;
+
+@DisplayName("Тестирование функции 'оплата без комиссии'")
+@Link(name = "MTS", url = "https://mts.by/")
+@Owner("Алексей Санин")
+@Severity(CRITICAL)
 public class MtsReplenishmentPageObjectTests {
 
     private WebDriver driver;
@@ -27,7 +42,9 @@ public class MtsReplenishmentPageObjectTests {
         if (driver != null) driver.quit();
     }
 
+
     @Test
+    @Step("Проверка переключений назначения платежа")
     void shouldCheckPlaceholdersForAllPaymentTypes() {
         MtsTopUpBlockPage page = new MtsTopUpBlockPage(driver).open();
 
@@ -37,7 +54,9 @@ public class MtsReplenishmentPageObjectTests {
         page.assertPlaceholdersFor("Задолженность");
     }
 
+
     @Test
+    @Step("Проверка оплаты 'Услуги связи'")
     void shouldCheckConnectionPaymentFlowAndIframeData() {
         String phone = "297777777";
         String sum = "10";
